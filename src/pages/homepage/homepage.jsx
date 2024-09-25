e58549acff87070ba960d5eb0085b6ff058280e1
prasnaaryal <prasna101@gmail.com>
2024-09-25 14:03:07 +0545

    homepage created

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import './homepage.scss';
+import Directory from "../../component/directory/directory";
+
+
+
+const Homepage = () => (
+  <div className="homepage">
+    <Directory/>
+  </div>
+);
+
+export default Homepage
